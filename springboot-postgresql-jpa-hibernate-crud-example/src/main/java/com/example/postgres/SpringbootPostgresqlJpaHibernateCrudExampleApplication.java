466191a56e2486e99d0c3bce7cada24f7a72d583
Sathees Kumar K <65384711+sathees-saty@users.noreply.github.com>
2024-03-30 06:58:28 +0530

    Add files via upload

---
+++ springboot-postgresql-jpa-hibernate-crud-example/src/main/java/com/example/postgres/SpringbootPostgresqlJpaHibernateCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlJpaHibernateCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresqlJpaHibernateCrudExampleApplication.class, args);
+	}
+
+}
---
+++ springboot-postgresql-jpa-hibernate-crud-example/src/test/java/com/example/postgres/SpringbootPostgresqlJpaHibernateCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresqlJpaHibernateCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
